2146115212addee229701934118096e81edf6b82
aigarskadikis <aigars.kadikis@gmail.com>
2021-01-06 21:17:41 +0200

    next

--- javascript/modify-json-attribute-because-of-condition.js
+++ javascript/modify-json-attribute-because-of-condition.js
@@ -1,4 +1,8 @@
-const json = '{"jsonrpc":"2.0","result":[{"HOSTNAME":"first","HOSTGROUP":"Linux servers"},{"HOSTNAME":"second","HOSTGROUP":"Windows servers"},{"HOSTNAME":"third","HOSTGROUP":"SNMP device"}]}';
+var json = '{"jsonrpc":"2.0","result":[{"{#HOSTNAME}":"first","{#HOSTGROUP}":"Linux servers"},{"{#HOSTNAME}":"second","{#HOSTGROUP}":"Windows servers"},{"{#HOSTNAME}":"third","{#HOSTGROUP}":"SNMP device"}]}';
+
+json = json
+.replace(/{#HOSTNAME}/gm,'HOSTNAME')
+.replace(/{#HOSTGROUP}/gm,'HOSTGROUP');
 
 // convert the json string to native JS Object
 var JsonObject = JSON.parse(json);
@@ -11,11 +15,11 @@ lenghtOfArray=results.length;
 // loop over the elements
 for(var row = 0; row < lenghtOfArray; row++) {
 
-if (results[row].HOSTNAME == 'first') {
-results[row].HOSTGROUP='kaste';
-}
-
+if (results[row].HOSTNAME == 'first') results[row].HOSTGROUP='kaste';
 	
 }
 
-return JSON.stringify(results);
+return JSON.stringify(results)
+.replace(/HOSTNAME/gm,'{#HOSTNAME}')
+.replace(/HOSTGROUP/gm,'{#HOSTGROUP}');
+;
