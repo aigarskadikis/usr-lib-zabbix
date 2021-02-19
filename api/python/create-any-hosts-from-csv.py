6d7784d89c1ea3c19b773ff4fdd9820bf72e370a
aigarskadikis <aigars.kadikis@gmail.com>
2021-02-19 23:19:35 +0200

    next

--- api/python/create-any-hosts-from-csv.py
+++ api/python/create-any-hosts-from-csv.py
@@ -38,14 +38,13 @@ for line in reader:
  
  # check if this host exists in zabbix
  if not zapi.host.get({"filter":{"host" :line['name']}}):
-  print (line['name'],"not yet registred")
+  print (line['name'],"not yet registred. will register now")
 
   # check if proxy exists
   if zapi.proxy.get({"output": "proxyid","selectInterface": "extend","filter":{"host":line['proxy']}}):
 
    # observe proxy ID:
    proxy_id=zapi.proxy.get({"output": "proxyid","selectInterface": "extend","filter":{"host":line['proxy']}})[0]['proxyid']
-   print (line['proxy'])
 
    # if proxy exists in instance then:
    if int(proxy_id)>0:
@@ -57,38 +56,22 @@ for line in reader:
      templateIDarray=[]
      
      for template in templates:
-       
-       # print human friendly template name
-       print (template)
 
        # get template ID and add it to array
        templateIDarray.append({"templateid":int(zapi.template.get({"filter" : {"name" : template}})[0]['templateid'])})
 
-     # print array which will be used for bulks
-     print (templateIDarray)
-
      # get all host group IDs. This will create a host group array
      groups=line['group'].split(";")
-     print (groups)
      
      hostGroupIDarray=[] 
      for hostGroup in groups:
-       print (hostGroup)
        
        # get all host group IDs and put it in array. this is required so we can create a host with one command
        hostGroupIDarray.append({"groupid":int(zapi.hostgroup.get({"filter":{"name":hostGroup}})[0]['groupid'])})
 
-     print (hostGroupIDarray)
-
      # create a host based on it's type in table
      if line['type']=='ZBX':
        print ("Zabbix agent hosts must be registered through functionality of agent auto registration")
-#       hostid = zapi.host.create ({
-#                            "host":line['name'],
-#                            "interfaces":[{"type":1,"dns":"","main":1,"ip": line['address'],"port": 10051,"useip": 1}],
-#                            "groups":hostGroupIDarray,
-#                            "proxy_hostid":proxy_id,
-#                            "templates":templateIDarray})['hostids']
 
      if line['type']=='SNMP':
        hostid = zapi.host.create ({
