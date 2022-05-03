53a7872494242c3d40d26b067f4bdf560877cc11
catonrug <aigars.kadikis@gmail.com>
2022-05-03 17:38:18 +0300

    next

--- php/import-yaml/delete_missing.php
+++ php/import-yaml/delete_missing.php
@@ -45,7 +45,11 @@ $response = file_get_contents($api_url, false, stream_context_create([
 			'method'  => 'POST',
 			'header'  => 'Content-Type: application/json',
 			'content' => $request
-	]]));
+	],'ssl' => [
+                        'allow_self_signed'     => true,
+                        'verify_peer'           => false,
+                        'verify_peer_name'      => false
+                ]]));
 
 $response = json_decode($response, true);
 
--- php/import-yaml/refresh_stock.sh
+++ php/import-yaml/refresh_stock.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-find $3 -type f -name '*.yaml' | \
+
+# remove old dir. start fresh
+rm -rf /tmp/6.0.zip
+
+rm -rf /tmp/zabbix-release-6.0
+
+# download latest 6.0 branch
+curl -kL https://github.com/zabbix/zabbix/archive/refs/heads/release/6.0.zip -o /tmp/6.0.zip
+
+# unzip
+cd /tmp
+unzip 6.0.zip
+
+# go back to previous directory where PHP program is located
+cd -
+
+# start template import
+find /tmp/zabbix-release-6.0/templates -type f -name '*.yaml' | \
 while IFS= read -r TEMPLATE
 do {
-php delete_missing.php $1 $2 $TEMPLATE
+php delete_missing.php 133c784408ab0e83e0ecc56816851d4281b19098be52b11ee95df5ec1fcc34d7 https://z60.aigarskadikis.com:44360/api_jsonrpc.php $TEMPLATE
 } done
+
